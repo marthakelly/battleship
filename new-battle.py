9e27b7790aad321dd85d2914d4f6dfa775fbe966
Martha Kelly <mk.girdler@gmail.com>
2012-07-11 18:37:28 -0400

    working horizontally

--- new-battle.py
+++ new-battle.py
@@ -7,26 +7,42 @@ def new_board(rows = 3, columns = 3, default_value = 0):
 	for i in range(rows):
 		board.append([default_value] * columns)
 	return board
-	
+
+battleship = new_board(10, 10)
+
 def fire_shot(x, y, default_value, board):
 	board[x][y] = default_value
 
-def place_ship(size, board):
-	rand_row = random.randrange(0, 9 - size)
-	rand_col = random.randrange(0, 9 - size)
+def find_open_spot(row, blank_space, ship, first_match=True):
+	i = random.randrange(0, 9 - ship)
+	while i < len(row):
+		if [blank_space]*ship == row[i:i + ship]:
+			row[i:i+ship] = [1]*ship
+			i += ship
+			if first_match:
+				break
+		else:
+			i += 1
+	return row
 
-	count = 0
-	while count < size:
-		board[rand_row][rand_col + count] = 1
-		count += 1
-		
-		
+def place_ship(ship):
+	i = random.randrange(0, 10)
+	row = battleship[i]
 	
+	add_ship = find_open_spot(row, 0, ship)
+	battleship[i] = add_ship
+	
+	print ship
 
-battleship = new_board(10, 10, 0)
+ship_list = [5, 4, 3, 3, 2]
 
-fire_shot(1, 1, 5, battleship)
-
-place_ship(3, battleship)
+for i in ship_list:
+	place_ship(i)
+	
+#place_ship(battleship)
 
 pprint(battleship)
+
+#fire_shot(1, 1, 5, battleship)
+
+# make sure ships dont overlap and have white space around them
