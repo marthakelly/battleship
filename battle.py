cd7f1a08b8d133df614b309dc63090d20b81bfd2
Martha Kelly <mk.girdler@gmail.com>
2012-07-12 14:18:29 -0400

    have coordinates correct now

--- battle.py
+++ battle.py
@@ -1,6 +1,12 @@
 from pprint import pprint
 import random
 
+aircraft_carrier = []
+battle_ship = []
+submarine = []
+destroyer = []
+patrol_boat = []
+
 def new_board(rows = 3, columns = 3, default_value = 0):
 	board = [] 
 	
@@ -8,26 +14,49 @@ def new_board(rows = 3, columns = 3, default_value = 0):
 		board.append([default_value] * columns)
 	return board
 
-def fire_shot(x, y, default_value, board):
-	board[x][y] = default_value
+def fire_shot():
+	print "Select x, y coordinates to fire a shot."
+	fire = input("You may fire when ready: ")
+	x = fire[0]
+	y = fire[1]
+	
+	if battleship[x][y] == 1:
+		print 'hit!'
+	else:
+		print 'miss!'
+		
+	battleship[x][y] = 5
+	
+	pprint(battleship)
+	
+	fire_shot()
 
-def find_open_spot(row, blank_space, ship, first_match=True):
-	i = random.randrange(0, 9 - ship)
-	while i < len(row):
-		if [blank_space]*ship == row[i:i + ship]:
-			row[i:i+ship] = [1]*ship
-			#i += ship
+def find_open_spot(row, i, blank_space, ship, first_match=True):
+	j = random.randrange(0, 9 - ship)
+	while j < len(row):
+		if [blank_space]*ship == row[j:j + ship]:
+			row[j:j+ship] = [1]*ship
 			if first_match:
 				break
 		else:
-			i += 1
+			j += 1
 	
-	placement = [j for j in range(len(row)) if row[j] == 1]
+	placement = [k for k in range(len(row)) if row[k] == 1]
 	
 	# get the index for this particular ship
-	
-	for k in placement:
-		print [i, k]
+
+	for l in placement:
+		if ship == 5:
+			aircraft_carrier.append([i, l])
+		elif ship == 4:
+			battle_ship.append([i, l])
+		elif ship == 3:
+			if len(submarine) < 3:
+				submarine.append([i, l])
+			else:
+				destroyer.append([i, l])
+		elif ship == 2:
+			patrol_boat.append([i, l])
 
 	return row
 
@@ -35,20 +64,25 @@ def place_ship(ship):
 	i = random.randrange(0, 10)
 	row = battleship[i]
 	
-	add_ship = find_open_spot(row, 0, ship)
+	add_ship = find_open_spot(row, i, 0, ship)
 	battleship[i] = add_ship
 
 battleship = new_board(10, 10)
 
 ships = [5, 4, 3, 3, 2]
-#ships = [[5, "destroyer"]]
 
 for ship in ships:
 	place_ship(ship)
 
 pprint(battleship)
 
-#fire_shot(1, 1, 5, battleship)
+print 'aircraft carrier', aircraft_carrier
+print 'battle ship', battle_ship
+print 'submarine', submarine
+print 'destroyer', destroyer
+print 'patrol boat', patrol_boat
+
+fire_shot()
 
 # to do:
 # work out vertical placement
