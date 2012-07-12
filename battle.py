a2bcaef7a0c87d07b46ae806b8951bcbb4e11faf
Martha Kelly <mk.girdler@gmail.com>
2012-07-12 17:27:47 -0400

    horizontal working for correct coordinates. still need to polish up vertical

--- battle.py
+++ battle.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 import random
+import sys
 
 aircraft_carrier = []
 battle_ship = []
@@ -14,27 +15,44 @@ def new_board(rows = 3, columns = 3, default_value = 0):
 		board.append([default_value] * columns)
 	return board
 
-
 def fire_shot():
 	print "Select x, y coordinates to fire a shot."
 	fire = input("You may fire when ready: ")
 	x = fire[0]
 	y = fire[1]
+
+	all_ships = [aircraft_carrier] + [battle_ship] + [submarine] + [destroyer] + [patrol_boat]
+	#print all_ships
 	
 	if battleship[x][y] == 1:
-		print 'hit!'
+		for ship in all_ships:
+			for spot in ship:
+				if spot == [x, y]:
+					ship.remove(spot)
+					print "hit!"
+		
+		if len(all_ships) == 0:
+			print "You Win!"
+			sys.exit()
+		
+		for ship in all_ships:
+			if len(ship) == 0:
+				if all_ships.index(ship) == 0:
+					print "You sunk the aircraft carrier!"
+				elif all_ships.index(ship) == 1:
+					print "You sunk the battle ship!"
+				elif all_ships.index(ship) == 2:
+					print "You sunk the submarine!"
+				elif all_ships.index(ship) == 3:
+					print "You sunk the destroyer!"
+				elif all_ships.index(ship) == 4:
+					print "You sunk the patrol boat!"
 	else:
-		print 'miss!'
+		print "miss!"
 		
 	battleship[x][y] = 5
-	
 	pprint(battleship)
-	
 	fire_shot()
-
-
-
-# new
 
 def transform_board(battleship):
     a = battleship
@@ -45,35 +63,38 @@ def transform_board(battleship):
 
     return battleship
 
-# new
-
-
 def find_open_spot(row, i, blank_space, ship, first_match=True):
+	#ship += 2
 	j = random.randrange(0, 9 - ship)
 	while j < len(row):
 		if [blank_space]*ship == row[j:j + ship]:
-			row[j:j+ship] = [1]*ship
+			row[j:j + ship] = [1]*ship
+			
+			placement = [k for k in range(len(row)) if row[k] == 'x']
+			
+			if ship == 5:
+				for coords in xrange(5):
+					aircraft_carrier.append([i, j+coords])
+			elif ship == 4:
+				for coords in xrange(4):
+					battle_ship.append([i, j+coords])
+			elif ship == 3:
+				if len(submarine) < 3:
+					for coords in xrange(3):
+						submarine.append([i, j+coords])
+				else:
+					for coords in xrange(3):
+						destroyer.append([i, j+coords])
+			elif ship == 2:
+				for coords in xrange(2):
+					patrol_boat.append([i, j+coords])
+					
+			i += ship
+			
 			if first_match:
 				break
 		else:
 			j += 1
-	
-	placement = [k for k in range(len(row)) if row[k] == 1]
-	
-	# get the index for this particular ship
-
-	for l in placement:
-		if ship == 5:
-			aircraft_carrier.append([i, l])
-		elif ship == 4:
-			battle_ship.append([i, l])
-		elif ship == 3:
-			if len(submarine) < 3:
-				submarine.append([i, l])
-			else:
-				destroyer.append([i, l])
-		elif ship == 2:
-			patrol_boat.append([i, l])
 
 	return row
 
@@ -85,19 +106,17 @@ def place_ship(ship):
 	battleship[i] = add_ship
 	
 	return battleship
-	
+
+#init battleship board
 battleship = new_board(10, 10)
 
 ships = [5, 4, 3, 3, 2]
 
 place_ship(ships[0])
-battleship = transform_board(battleship)
+#battleship = transform_board(battleship)
 place_ship(ships[1])
-battleship = transform_board(battleship)
 place_ship(ships[2])
-battleship = transform_board(battleship)
 place_ship(ships[3])
-battleship = transform_board(battleship)
 place_ship(ships[4])
 
 pprint(battleship)
